e964a218106344ffb6200eb7eb72f0ce223b517a
AnhuarA <aaabrajan@cpp.edu>
2016-11-23 11:35:23 -0800

    Finished Determine state function.

--- PCTS.cpp
+++ PCTS.cpp
@@ -8,6 +8,7 @@ using namespace std;
 PCTS::PCTS()
 {
 	bool lightStatus[3] = {true, true, true}; // Instantiate values to all true
+	state = 0;
 }
 
 void PCTS::contact_division()// defining fucntinos
@@ -21,8 +22,32 @@ void PCTS::Start_Calculations()
 }
 int PCTS::Determine_State(bool x, bool y, bool z)
 {
+	if(x == true && y == true && z == true){
+		state = 0;
+	}
+		else if(x == true && y == true && z == false){
+			state = 1;
+		}
+			else if(x == true && y == false && z == true){
+				state = 2;
+			}
+				else if(x == true && y == false && z == false){
+					state = 3;
+				}
+					else if(x == false && y == true && z == true){
+						state = 4;
+					}
+						else if(x == false && y == true && z == false){
+							state = 5;
+						}
+							else if(x == false && y == false && z == true){
+								state = 6;
+							}
+								else if(x == false && y == false && z == false){
+									state = 6;
+								}	
 
-
+	return state;
 }
 void PCTS::New_State()
 {
