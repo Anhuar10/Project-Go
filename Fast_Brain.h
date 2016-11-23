944871564ce5139ea76cfe715f7f76486dd322f2
Someguy63 <froylan.mondragon@yahoo.com>
2016-11-23 10:47:55 -0800

    Update Fast_Brain.h

--- Fast_Brain.h
+++ Fast_Brain.h
@@ -1,1 +1,20 @@
 #pragma once
+#ifndef FAST_BRAIN_H
+#define FAST_BRAIN_H
+
+class fast_brain: public PCTS// trying to do inheritence, PCTS is Base, slow_brain derived 
+{
+public:
+	fast_brain();
+
+private:
+	int MTBF;
+	double criticality;
+	int priority[];
+
+};
+
+
+
+
+#endif 
