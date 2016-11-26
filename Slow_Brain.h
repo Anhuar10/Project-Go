f8e15894e63687f1953f53b0cf72927a43af6d22
matthew-novoa <matthew.novoa@icloud.com>
2016-11-25 20:22:24 -0800

    Split .cpp and .h
    Worked on the submissions of Roshane. I split the functions and data members into .h

--- Slow_Brain.h
+++ Slow_Brain.h
@@ -2,16 +2,21 @@
 #ifndef SLOW_BRAIN_H
 #define SLOW_BRAIN_H
 
-class Slow_Brain: public PCTS// trying to do inheritence, PCTS is Base, slow_brain derived 
+class Slow_Brain : public Fast_Brain // trying to do inheritence, PCTS is Base, slow_brain derived 
 {
 public:
-	Slow_Brain();
+	// Constructor
+	Slow_Brain(int current_state, double input_reliability[])
+	{
+		state = current_state;
+		*reliability = input_reliability[9];//allocating 9 element spaces with "reliability being the pointer"
+	};
+	double Calculate_Criticality(Slow_Brain s);
+	int Calculate_Priority(Slow_Brain s);
 
-private:
-	int MTBF;
-	double criticality;
-	int priority[];
-
+protected:
+	int state;
+	double *reliability;// pointer to an element of an array 
 };
 
 
