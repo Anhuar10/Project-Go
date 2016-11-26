ce52ce05448c73930c2e8d01b6ab71e2f5ced8d9
Someguy63 <froylan.mondragon@yahoo.com>
2016-11-25 16:01:16 -0800

    Update Slow_Brain.h

--- Slow_Brain.h
+++ Slow_Brain.h
@@ -2,10 +2,10 @@
 #ifndef SLOW_BRAIN_H
 #define SLOW_BRAIN_H
 
-class slow_brain: public PCTS// trying to do inheritence, PCTS is Base, slow_brain derived 
+class Slow_Brain: public PCTS// trying to do inheritence, PCTS is Base, slow_brain derived 
 {
 public:
-	slow_brain();
+	Slow_Brain();
 
 private:
 	int MTBF;
