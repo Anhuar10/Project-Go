3dd9e5d90208cca9af156771f79498ac80fec91a
AnhuarA <aaabrajan@cpp.edu>
2016-11-26 17:57:42 -0800

    main file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Project Go
+//
+//  Created by Anhuar Abrajan on 11/9/16.
+//  Copyright (c) 2016 Anhuar Abrajan. All rights reserved.
+//
+#include "PCTS.h"
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    PCTS Pomona(true, false, false);
+    
+    system("pause");
+    return 0;
+}
